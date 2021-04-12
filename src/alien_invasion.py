d4fba382231286eab1a5ff20750bec2adf4dc340
dassaswat <68921527+dassaswat@users.noreply.github.com>
2021-04-12 21:13:27 +0530

    Addition of the ship and firing control.
    Image of ship has been added. Piloting function for the ship as well as the firing function of the ship is provided.

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -1,23 +1,27 @@
-import sys
 import pygame
+
 from settings import Settings
+import game_functions as gf
+from ship import Ship
+from pygame.sprite import Group
 
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
+
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+
     while True:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-        pygame.display.flip()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
--- src/settings.py
+++ src/settings.py
@@ -4,3 +4,11 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 1.5
+
+        # Bullet Settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 6
+        self.bullet_height = 10
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 6
